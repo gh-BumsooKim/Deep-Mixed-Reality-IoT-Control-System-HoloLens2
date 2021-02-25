4c8f51d4be365efbe782238364344d090fa0d739
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2021-02-25 18:11:50 +0900

    Create client_tcp.cs

---
+++ client_tcp.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+public class Socket_01 : MonoBehaviour
+{
+    public void socket_client()
+    {
+        Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        client.Connect(new IPEndPoint(IPAddress.Parse("192.168.0.11"), 43210));
+
+        var data = Encoding.UTF8.GetBytes("Hello");
+
+        //client.Send(BitConverter.GetBytes(data.Length));
+        client.Send(data);
+
+        data = new byte[4];
+        //client.Receive(data, data.Length, SocketFlags.None);
+        //Array.Reverse(data);
+        //data = new byte[BitConverter.ToInt32(data, 0)];
+        client.Receive(data, data.Length, SocketFlags.None);
+        Debug.Log(Encoding.UTF8.GetString(data));
+    }
+}
