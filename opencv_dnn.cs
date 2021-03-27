e6d551c0c4cfabd443f86863ccd24053294b3690
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2021-03-27 15:15:54 +0900

    Create opencv_dnn.cs

---
+++ opencv_dnn.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+public class opencv_dnn : MonoBehaviour
+{
+    public void is_obj_detected()
+    {
+        
+    }
+}
